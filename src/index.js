6156735629e5660887532de0cd0794ac707e309f
Fredyic <fredylpa@gmail.com>
2024-03-13 21:07:14 +0000

    add html carta

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const products = document.getElementsByClassName('product-container')
+
+const boton1 = document.getElementById("add1")
+//console.log(boton1)
+boton1.addEventListener("click", addCarrito)
+
+
+
+function addCarrito (product,price){
+    console.log("click")
+}
+
+//console.log(products)
