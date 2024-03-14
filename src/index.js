87275685cc82fdab0100332afd439aef298c77df
Fredyic <fredylpa@gmail.com>
2024-03-14 18:15:24 +0000

    subidos nuevos archivos para el carrito

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 const products = document.getElementsByClassName('product-container')
 
-const boton1 = document.getElementById("add1")
-//console.log(boton1)
-boton1.addEventListener("click", addCarrito)
+const botones = document.getElementsByTagName("button")
+
+
+
+for (i=0;i<botones.length;i++){
+    if (botones[i].addEventListener("click","nada")){
+        console.log("BOTON1")
+    }
+}
+/*
+for (i=0;i< products.length;i++){
+    console.log (products[i].innerHTML)
+}
+*/
+//console.log(botones)
+
+//botones.addEventListener("click", addCarrito)
 
 
 
